dd8d5d84e257c4c9c05ad8feeb1061cda44d1019
shubh947 <shubhangbanerjee03@gmail.com>
2026-07-07 20:10:54 +0530

    print

---
+++ print.py
@@ -0,0 +1,8 @@
+print("Welcome to python")
+name = input("What is your name?")
+print("Hello", name )
+answer = input("Is python a programming language?")
+if answer.lower() == "yes":
+  print("Correct")
+else:
+  print("Oops. Python is a programming language")
