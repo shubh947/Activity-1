1a060206b84c3876cc88497bed0a80619f361605
shubh947 <shubhangbanerjee03@gmail.com>
2026-07-07 20:12:57 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,6 @@
+name = input("Enter your name")
+print("My name is", name)
+if name == "shubhang":
+   print("I am a human")
+else:
+   print("I am not a human either a alien or a robot or I don't exist")
